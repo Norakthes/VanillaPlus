6a33e212b270ea3febc8c37d3f7b81834d27de6c
Rasmus Bødker <rasmus.b.bodker@gmail.com>
2020-08-03 16:07:03 +0200

    Too tired to fix my shitty code

--- src/main/java/xyz/norakthes/vanillaplus/VanillaPlus.java
+++ src/main/java/xyz/norakthes/vanillaplus/VanillaPlus.java
@@ -3,24 +3,48 @@ package xyz.norakthes.vanillaplus;
 import de.tr7zw.changeme.nbtapi.NBTCompound;
 import de.tr7zw.changeme.nbtapi.NBTCompoundList;
 import de.tr7zw.changeme.nbtapi.NBTItem;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class VanillaPlus extends JavaPlugin {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class VanillaPlus extends JavaPlugin implements Listener {
 //    public static ItemStack setAttackSpeed(ItemStack weapon, float amount) {
 //
 //    }
 
-    static ItemStack emeraldSword = new ItemStack(Material.DIAMOND_SWORD);
+    static ItemStack emeraldSword = new ItemStack(Material.DIAMOND_SWORD, 1);
+
+    static int emeraldSwordDurability = 941;
 
     static {
-        emeraldSword.getItemMeta().setDisplayName("Emerald Sword");
-//        emeraldSword.getItemMeta().addAttributeModifier(Attribute.GENERIC_ATTACK_SPEED, new AttributeModifier("generic.attackSpeed",20, AttributeModifier.Operation.ADD_NUMBER));
+        NBTItem nbti = new NBTItem(emeraldSword);
+        ItemMeta emeraldSwordMeta = emeraldSword.getItemMeta();
+        ArrayList<String> lore = new ArrayList<>();
+
+        lore.add("Durability: " + emeraldSwordDurability);
+
+        emeraldSwordMeta.setDisplayName("§fEmerald Sword");
+        emeraldSwordMeta.setCustomModelData(1);
+
+        emeraldSwordMeta.setLore(lore);
+        emeraldSword = nbti.getItem();
+        emeraldSword.setItemMeta(emeraldSwordMeta);
     }
 
     static ShapedRecipe emeraldSwordRecipe = new ShapedRecipe(NamespacedKey.minecraft("vanillaplus"), emeraldSword).shape(
@@ -29,17 +53,14 @@ public final class VanillaPlus extends JavaPlugin {
             " - ")
             .setIngredient('*', Material.EMERALD).setIngredient('-',Material.STICK);
 
-    static boolean compound;
-
-
 
     @Override
     public void onEnable() {
 
+        getServer().getPluginManager().registerEvents(this,this);
+
         getServer().addRecipe(emeraldSwordRecipe);
 
-        this.getCommand("compound").setExecutor(new compoundDebug());
-        this.getCommand("test").setExecutor(new test());
         this.getCommand("NBT").setExecutor(new NBT());
 
     }
@@ -47,5 +68,25 @@ public final class VanillaPlus extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerItemDamage(PlayerItemDamageEvent event){
+        if (event.getItem().getItemMeta().getCustomModelData() == 1 && event.getItem().getType() == Material.DIAMOND_SWORD){
+            ItemMeta itemMeta = event.getItem().getItemMeta();
+            List<String> lore = event.getItem().getLore();
+            Bukkit.broadcastMessage(String.valueOf(lore.get(1)));
+            int durabilityInt;
+//            if (Integer.parseInt(durability)  != -1){
+//                durabilityInt = Integer.parseInt(durability);
+//                durabilityInt--;
+//                lore.add("Durability: " + durabilityInt);
+//            }
+//            itemMeta.setLore(lore);
+//            event.getItem().setItemMeta(itemMeta);
+            event.setCancelled(true);
+
+
+        }
     }
 }
