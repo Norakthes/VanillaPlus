d5c3b543a5e255bfcece7bb1e16095d74ecdf99d
rasmus <rasmus.b.bodker@gmail.com>
2020-08-12 10:00:43 +0200

    bruh

--- src/main/java/xyz/norakthes/vanillaplus/VanillaPlus.java
+++ src/main/java/xyz/norakthes/vanillaplus/VanillaPlus.java
@@ -1,26 +1,18 @@
 package xyz.norakthes.vanillaplus;
 
-import de.tr7zw.changeme.nbtapi.NBTCompound;
-import de.tr7zw.changeme.nbtapi.NBTCompoundList;
 import de.tr7zw.changeme.nbtapi.NBTItem;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.attribute.AttributeModifier;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public final class VanillaPlus extends JavaPlugin implements Listener {
@@ -74,26 +66,42 @@ public final class VanillaPlus extends JavaPlugin implements Listener {
         ItemStack itemStack = event.getItem();
         int customModelData = event.getItem().getItemMeta().getCustomModelData();
 
-        switch (itemMaterial) {
-            case DIAMOND_SWORD:
-                List<String> lore = event.getItem().getLore();
-                assert lore != null;
-                String[] loreArray = new String[lore.size()];
-                lore.toArray(loreArray);
-                switch (customModelData){
-                    case 1:
-                        int itemMeta = Integer.parseInt(loreArray[1]);
-                        ArrayList<String> swordLore = new ArrayList<>();
-                        itemMeta--;
 
+        if (itemMaterial == Material.DIAMOND_SWORD && customModelData == 1){
+            List<String> lore = event.getItem().getLore();
+            assert lore != null;
+            String[] loreArray = new String[lore.size()];
+            lore.toArray(loreArray);
+            int damage = Integer.parseInt(loreArray[1]);
 
+            loreArray[1] = String.valueOf((damage = damage--));
 
-                        swordMeta.setLore(swordLore);
-                        event.setCancelled(true);
-                        itemStack.setItemMeta(swordMeta);
-                        break;
-                }
-                break;
+            lore = Arrays.asList(loreArray);
+                swordMeta.setLore(lore);
+                itemStack.setItemMeta(swordMeta);
+            event.setCancelled(true);
         }
+            //Future implementation
+//        switch (itemMaterial) {
+//            case DIAMOND_SWORD:
+//                List<String> lore = event.getItem().getLore();
+//                assert lore != null;
+//                String[] loreArray = new String[lore.size()];
+//                lore.toArray(loreArray);
+//                switch (customModelData){
+//                    case 1:
+//                        int itemMeta = Integer.parseInt(loreArray[1]);
+//                        ArrayList<String> swordLore = new ArrayList<>();
+//                        itemMeta--;
+//
+//
+//
+//                        swordMeta.setLore(swordLore);
+//                        event.setCancelled(true);
+//                        itemStack.setItemMeta(swordMeta);
+//                        break;
+//                }
+//                break;
+//        }
     }
 }
