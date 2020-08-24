aa978bc8a8f8b79a93921e6ba902162749cb9993
Rasmus Bødker <rasmus.b.bodker@gmail.com>
2020-08-24 21:48:22 +0200

    Implementing support for unbreaking enchantment, and implementing basics for an emerald pickaxe

--- src/main/java/xyz/norakthes/vanillaplus/VanillaPlus.java
+++ src/main/java/xyz/norakthes/vanillaplus/VanillaPlus.java
@@ -1,8 +1,8 @@
 package xyz.norakthes.vanillaplus;
 
-import de.tr7zw.changeme.nbtapi.NBTItem;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,19 +15,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public final class VanillaPlus extends JavaPlugin implements Listener {
 
     static ItemStack emeraldSword = new ItemStack(Material.DIAMOND_SWORD, 1);
+    static ItemStack emeraldPickaxe = new ItemStack(Material.DIAMOND_PICKAXE, 1);
 
-    static int emeraldSwordDurability = 941; //TODO make durability configurable
+    static int emeraldDurability = 941; //TODO make durability configurable
 
-    //  Emerald sword
+    // Emerald sword configuration
     static {
-        NBTItem nbti = new NBTItem(emeraldSword);
         ItemMeta emeraldSwordMeta = emeraldSword.getItemMeta();
         ArrayList<String> lore = new ArrayList<>();
-        lore.add("§fDurability: " + emeraldSwordDurability +" / "+ emeraldSwordDurability);
+        lore.add("§fDurability: " + emeraldDurability +" / "+ emeraldDurability);
 
         emeraldSwordMeta.setDisplayName("§fEmerald Sword");
         emeraldSwordMeta.setCustomModelData(1);
@@ -36,12 +37,32 @@ public final class VanillaPlus extends JavaPlugin implements Listener {
         emeraldSword.setItemMeta(emeraldSwordMeta);
     }
 
+    // Emerald pickaxe configuration
+    static {
+        ItemMeta emeraldPickaxeMeta = emeraldPickaxe.getItemMeta();
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add("§fDurability: " + emeraldDurability +" / "+ emeraldDurability);
+
+        emeraldPickaxeMeta.setDisplayName("§fEmerald Pickaxe");
+        emeraldPickaxeMeta.setCustomModelData(1);
+
+        emeraldPickaxeMeta.setLore(lore);
+        emeraldPickaxe.setItemMeta(emeraldPickaxeMeta);
+    }
+
     static ShapedRecipe emeraldSwordRecipe = new ShapedRecipe(NamespacedKey.minecraft("vanillaplus"), emeraldSword).shape(
             " * ",
             " * ",
             " - ")
             .setIngredient('*', Material.EMERALD)
-            .setIngredient('-',Material.STICK);
+            .setIngredient('-', Material.STICK);
+
+    static ShapedRecipe emeraldPickaxeRecipe = new ShapedRecipe(NamespacedKey.minecraft("vanillaplus"), emeraldPickaxe).shape(
+            "***",
+            " - ",
+            " - ")
+            .setIngredient('*', Material.EMERALD)
+            .setIngredient('-', Material.STICK);
 
 
     @Override
@@ -50,8 +71,9 @@ public final class VanillaPlus extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this,this);
 
         getServer().addRecipe(emeraldSwordRecipe);
+        getServer().addRecipe(emeraldPickaxeRecipe);
 
-        this.getCommand("NBT").setExecutor(new NBT());
+        Objects.requireNonNull(this.getCommand("NBT")).setExecutor(new NBT());
 
     }
 
@@ -65,15 +87,29 @@ public final class VanillaPlus extends JavaPlugin implements Listener {
         Material itemMaterial = event.getItem().getType();
         ItemStack itemStack = event.getItem();
         ItemMeta itemMeta = null;
-        int customModelData = 0;
-        List<String> lore = null;
+        List<String> lore;
+        int currentDurability = 0;
 
         if (event.getItem().hasItemMeta()){
             itemMeta = event.getItem().getItemMeta();
             if (event.getItem().getItemMeta().hasLore()){
                 lore = event.getItem().getLore();
+                String[] loreArray = lore.toString().split(" ");
+                currentDurability = Integer.parseInt(loreArray[1]);
             }
         }
+
+        Player player = event.getPlayer();
+        boolean hasEnchantment = event.getItem().containsEnchantment(Enchantment.DURABILITY);
+        int durabilityLevel;
+        float percentage = 0;
+
+        if (hasEnchantment){
+            durabilityLevel = event.getItem().getEnchantmentLevel(Enchantment.DURABILITY);
+             percentage = 100 / ((float) durabilityLevel + 1);
+        }
+
+        int customModelData = 0;
 
         if (event.getItem().getItemMeta().hasCustomModelData()){
             customModelData = event.getItem().getItemMeta().getCustomModelData();
@@ -85,26 +121,36 @@ public final class VanillaPlus extends JavaPlugin implements Listener {
 //            int currentDurability = Integer.parseInt(loreArray[1]);
 //
 //            currentDurability--;
-//            itemMeta.setLore(Collections.singletonList(ChatColor.DARK_GRAY + "§fDurability: " + currentDurability + " / " + emeraldSwordDurability));
+//            itemMeta.setLore(Collections.singletonList("§fDurability: " + currentDurability + " / " + emeraldSwordDurability));
 //            itemStack.setItemMeta(itemMeta);
 //            player.getInventory().setItemInMainHand(itemStack);
 //            event.setCancelled(true);
 //        }
-            //Future implementation
         switch (itemMaterial) {
             case DIAMOND_SWORD:
                 switch (customModelData) {
                     case 1:
-                        String[] loreArray = lore.toString().split(" ");
-                        Player player = event.getPlayer();
-                        int currentDurability = Integer.parseInt(loreArray[1]);
 
-                        currentDurability--;
-                        itemMeta.setLore(Collections.singletonList("§fDurability: " + currentDurability + " / " + emeraldSwordDurability));
+
+                        if (hasEnchantment){
+                            double randomNum = Math.random()*100;
+
+                            if (randomNum > percentage){
+                                currentDurability--;
+                            }
+                        }
+                        else {currentDurability--;}
+
+                        itemMeta.setLore(Collections.singletonList("§fDurability: " + currentDurability + " / " + emeraldDurability));
                         itemStack.setItemMeta(itemMeta);
                         player.getInventory().setItemInMainHand(itemStack);
                         event.setCancelled(true);
                         break;
+                }
+            case DIAMOND_PICKAXE:
+                switch (customModelData) {
+                    case 1:
+
                 }
         }
     }
